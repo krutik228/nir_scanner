2132f391610c09309a6dd9a828f5599bd427ab79
n.krutikov <n.krutikov@okko.tv>
2023-09-05 02:35:37 +0700

    fix

--- app/versioneer/utils.py
+++ app/versioneer/utils.py
@@ -8,3 +8,15 @@ def subprocess_run(command):
         check=True,
         capture_output=True,
     ).stdout.decode()
+
+
+def powershell_subprocess_run(command):
+    command = ['powershell', command]
+    proc = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        shell=True,
+        creationflags=0x08000000
+    )
+    proc.wait()
+    return proc.stdout.read().decode()
--- app/versioneer/windows/commanders/openvpn.py
+++ app/versioneer/windows/commanders/openvpn.py
@@ -2,16 +2,16 @@ import re
 from typing import Dict
 
 from app.utils.exceptions import VersionNotFound
-from app.versioneer.utils import subprocess_run
+from app.versioneer.utils import powershell_subprocess_run
 
 command = (
-    '$OpenVPN = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -match \'OpenVPN\' } | Select-Object -Property DisplayName, DisplayVersion, PSChildName; $OpenVPN.DisplayVersion'
+r'$OpenVPN = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -match "OpenVPN" } | Select-Object -Property DisplayName, DisplayVersion, PSChildName; $OpenVPN.DisplayVersion'
 )
 pattern = re.compile(r'(?P<version>[0-9\.]+)').match
 
 
 def openvpn_command(soft: str) -> Dict[str, str]:
-    raw_version: str = subprocess_run(command)
+    raw_version: str = powershell_subprocess_run(command)
     matched = pattern(raw_version)
     if matched:
         return {"version": matched.group("version")}
